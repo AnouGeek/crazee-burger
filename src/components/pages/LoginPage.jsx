7195e40f81ae99da2a4d341667092ec12d3d84a1
Anou pro <anou.webu@gmail.com>
2026-02-27 19:18:38 +0100

    ✨F01: LOGIN > Add title and form

--- src/components/pages/LoginPage.jsx
+++ src/components/pages/LoginPage.jsx
@@ -1,3 +1,38 @@
+import { useState } from "react";
+
 export default function LoginPage() {
-  return <div>LoginPage</div>;
+  // state
+  const [inputValue, setInputValue] = useState("");
+
+  // comportements
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert(`Bonjour ${inputValue}`);
+    setInputValue("");
+  };
+
+  const handleChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  // affichage (render)
+  return (
+    <div>
+      <h1 className="text-3xl font-bold mb-12">Bienvenue chez nous</h1>
+      <h2 className="text-2xl font-bold mb-4">Connectez-vous</h2>
+      <form action="submit" onSubmit={handleSubmit}>
+        <input
+        className="border px-1"
+          value={inputValue}
+          onChange={handleChange}
+          type="text"
+          placeholder="Entrez votre prénom..."
+          required
+        />
+        <button
+        className="border px-2 bg-slate-200 rounded cursor-pointer"
+        >Accédez à votre espace</button>
+      </form>
+    </div>
+  );
 }
