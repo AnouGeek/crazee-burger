2167f36624c637b86a6510b9d45dfeb84cac9655
Anou pro <anou.webu@gmail.com>
2026-03-02 16:26:37 +0100

    ✨F02: LOGIN > add redirection from/to ErrorPage and button style

--- src/components/pages/order/OrderPage.jsx
+++ src/components/pages/order/OrderPage.jsx
@@ -4,7 +4,7 @@ export default function OrderPage() {
   return (
     <div>
       <h1>OrderPage</h1>
-      <button>
+      <button className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 transition">
         <Link to="/">Déconnexion</Link>
       </button>
     </div>
