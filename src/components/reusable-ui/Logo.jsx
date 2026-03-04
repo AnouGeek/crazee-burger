66e691488b1f007437ed45f54650ead88ad7211c
Anou pro <anou.webu@gmail.com>
2026-03-04 16:19:16 +0100

    💄F03: LOGO > Add styling + fonts

--- src/components/reusable-ui/Logo.jsx
+++ src/components/reusable-ui/Logo.jsx
@@ -1,3 +1,19 @@
 export default function Logo() {
-  return <div className="bg-blue-800">LOGO CRAZEE BURGER</div>;
+  return (
+    <div className="flex items-center transform scale-[2.5] border border-blue-700">
+      <h1 className="text-[36px] leading-[1em] font-bold uppercase tracking-[1.5px] text-[#ffa01b] font-amatic">
+        CRAZEE
+      </h1>
+      
+      <img 
+        src="/images/logo-orange.png" 
+        alt="logo-crazee-burger" 
+        className="object-contain h-[60px] w-[80px] mx-[5px]"
+      />
+      
+      <h1 className="text-[36px] leading-[1em] font-bold uppercase tracking-[1.5px] text-[#ffa01b] font-amatic">
+        BURGER
+      </h1>
+    </div>
+  )
 }
