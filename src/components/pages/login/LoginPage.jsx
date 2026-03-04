f8714cc11bfad2ce7d23571813fc3cfc1d8a935d
Anou pro <anou.webu@gmail.com>
2026-03-04 15:11:41 +0100

    💄F03: LOGIN > Add styling and background

--- src/components/pages/login/LoginPage.jsx
+++ src/components/pages/login/LoginPage.jsx
@@ -5,7 +5,7 @@ import Logo from "../../reusable-ui/Logo";
 export default function LoginPage() {
   // affichage (render)
   return (
-    <div className="bg-red-600 h-screen flex justify-center items-center flex-col">
+    <div className="bg-black/50 h-screen flex justify-center items-center flex-col bg-[url('/images/burger-and-fries-background.jpg')] bg-cover bg-blend-darken absolute inset-0">
       <Logo />
       <LoginForm />
     </div>
