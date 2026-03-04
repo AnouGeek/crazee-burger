e9c0f407de395841b0b7809879da7144c5a03e72
Anou pro <anou.webu@gmail.com>
2026-03-04 16:27:30 +0100

    💄F03: LOGIN > LoginForm > Add styling

--- src/components/pages/login/LoginForm.jsx
+++ src/components/pages/login/LoginForm.jsx
@@ -20,10 +20,11 @@ export default function LoginForm() {
   // affichage (render)
   return (
     <div 
-    className="bg-green-500"
+    className="bg-green-500 text-center max-w-[500px] min-w-[400px] mx-auto py-[2.5rem] px-[2rem] rounded-[5px] font-amatic"
     action="submit" onSubmit={handleSubmit}>
-      <h1 className="text-3xl font-bold mb-12">Bienvenue chez nous !</h1>
-      <h2 className="text-2xl font-bold mb-4">Connectez-vous</h2>
+      <h1 className="text-white text-[48px] font-bold uppercase mt-[40px]">Bienvenue chez nous !</h1>
+      <hr className="border-[1.5px] border-solid border-[#f56a2c] mb-[40px]"/>
+      <h2 className="text-white text-[36px] font-bold mt-[20px] mx-[10px] mb-[10px]">Connectez-vous</h2>
       <input
         className="border px-1"
         value={inputValue}
